22aef69d897102551fb9ba26481b22d022c3224d
Pablo Calvo <pablocalvo1@gmail.com>
2021-11-04 20:24:15 -0300

    elregreso

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(process.env.PORT || 3000);
+// / "public" / css/styles.css
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "css")));
+
+//localhost:3000/
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
